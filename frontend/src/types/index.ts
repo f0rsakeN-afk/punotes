214e81fe64653080db8539523d15a10ee01320d7
f0rsakeN-afk <nar3sh0@gmail.com>
2024-12-12 10:38:21 +0545

    added types

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,19 @@
+export interface AssignMentTypes {
+  title: string;
+  subject: string;
+  url: string;
+  id: number;
+}
+
+export interface PYQTypes {
+  id: number;
+  title: string;
+  url: string;
+}
+
+export interface TeamMember {
+  name: string;
+  role: string;
+  image: string;
+  linkedin: string;
+}
