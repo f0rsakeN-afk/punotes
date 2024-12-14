13a191a38dab8812f952d80d6afb4260e933cbb7
f0rsakeN-afk <nar3sh0@gmail.com>
2024-12-14 21:23:12 +0545

    added new types

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -3,12 +3,14 @@ export interface AssignMentTypes {
   subject: string;
   url: string;
   id: number;
+  uploaded_at: Date;
 }
 
 export interface PYQTypes {
   id: number;
   title: string;
   url: string;
+  uploaded_at:Date;
 }
 
 export interface TeamMember {
@@ -16,4 +18,26 @@ export interface TeamMember {
   role: string;
   image: string;
   linkedin: string;
+}
+
+export interface PDFTypes {
+  id: string;
+  name: string;
+  shortName: string;
+  semesters: {
+    id: string;
+    name: string;
+    code: string;
+    credit: number;
+    notes: {
+      id: string;
+      title: string;
+      description: string;
+      fileUrl: string;
+      uploadDate: Date;
+      fileType: string;
+      fileSize: string;
+      downloads: number;
+    }[];
+  }[];
 }
