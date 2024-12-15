8271fe82421c3af33bff23168cc076c00dc3a13e
f0rsakeN-afk <nar3sh0@gmail.com>
2024-12-15 15:53:58 +0545

    added sprtOptions

---
+++ frontend/src/utils/sortOptions.ts
@@ -0,0 +1,23 @@
+ export interface sortOptionInterface {
+  value: string;
+  label: string;
+}
+
+export const sortOptions: sortOptionInterface[] = [
+  {
+    value: "title_asc",
+    label: "Title (A-Z)",
+  },
+  {
+    value: "title_desc",
+    label: "Title (Z-A)",
+  },
+  {
+    value: "uploaded_at_asc",
+    label: "Uploaded At (Newest)",
+  },
+  {
+    value: "uploaded_at_desc",
+    label: "Uploaded At (Oldest)",
+  },
+];
