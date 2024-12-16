58fc21fa7f120918532a5aa7e56f1e48a59d1669
f0rsakeN-afk <nar3sh0@gmail.com>
2024-12-16 10:08:21 +0545

    modified roles

---
+++ frontend/src/data/TeamData.ts
@@ -0,0 +1,32 @@
+import nishant from "../assets/nishant.jpeg";
+import naresh from "../assets/naresh.jpg";
+import pushkar from "../assets/pushkar.jpeg";
+import rakesh from "../assets/rakesh.jpeg";
+import { TeamMember } from "@/types";
+
+export const teamMembers: TeamMember[] = [
+  {
+    name: "Nishant Bishwakarma",
+    role: "Project Lead",
+    image: nishant,
+    linkedin: "https://www.linkedin.com/in/nishant-bishwakarma-4631222b9/",
+  },
+  {
+    name: "Pushkar Khawas",
+    role: "Technical Lead",
+    image: pushkar,
+    linkedin: "https://www.linkedin.com/in/pushkar-khawas-22256830a/",
+  },
+  {
+    name: "Naresh Rajbanshi",
+    role: " MERN Developer",
+    image: naresh,
+    linkedin: "https://www.linkedin.com/in/f0rsaken/",
+  },
+  {
+    name: "Rakesh Ray",
+    role: "Python Developer",
+    image: rakesh,
+    linkedin: "https://www.linkedin.com/in/rakesh-ray-001364341/",
+  },
+];
