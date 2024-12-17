a4b7cb01447700870de5b4d3ae3a84188a8b4231
f0rsakeN-afk <nar3sh0@gmail.com>
2024-12-17 09:40:27 +0545

    added assignment data

---
+++ frontend/src/data/AssignmentData.ts
@@ -0,0 +1,11 @@
+import { AssignMentTypes } from "@/types";
+
+export const assignmentData: AssignMentTypes[] = [
+  {
+    id: 1,
+    title: "",
+    subject: "",
+    url: "",
+    uploaded_at: new Date("2024-12-14"),
+  },
+];
