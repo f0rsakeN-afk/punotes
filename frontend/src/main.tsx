4bc34fec0eb86d73791827aac104bcf1fff378aa
f0rsakeN-afk <nar3sh0@gmail.com>
2024-12-18 19:37:16 +0545

    a minor changes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./scroll.css"
 import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./context/themeProvider.tsx";
-import { Provider } from 'react-redux'
+import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
