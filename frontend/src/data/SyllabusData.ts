dad8f1abb42b50daa569f7778db46238b1b514fe
puskar37 <pushkarkhawas65@gmail.com>
2025-01-07 11:48:25 +0545

    Syllabus of computer first and fourth and civil first

--- frontend/src/data/SyllabusData.ts
+++ frontend/src/data/SyllabusData.ts
@@ -37,4 +37,21 @@ export const SyllabusData: SyllabusTypes[] = [
     branch: "Civil",
     url: "https://drive.google.com/file/d/181UIgQNIBAcQLtA13jMhnraw4uyDg3aA/view?usp=drive_link",
   },
+  {
+    id: 7,
+    name: "First semester Computer",
+    branch: "Computer",
+    url: "https://drive.google.com/file/d/1bnbt88xp47f9pBbwGEcoSEYgtTFmBMcP/view?usp=drive_link"
+  },
+  {
+    id: 8,
+    name: "Fourth semester Computer",
+    branch: "Computer",
+    url: "https://drive.google.com/file/d/1LEIQENg0sphJyKBpAxz1OEHp1k-l1XF-/view?usp=drive_link"
+  },{
+    id: 9,
+    name: "First semester Civil",
+    branch: "Civil",
+    url: "https://drive.google.com/file/d/1_6X2IRVK328CEp2448MSW8rUHKm8uOy8/view?usp=drive_link"
+  },
 ];
