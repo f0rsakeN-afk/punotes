06581aaafbdb45367cd9fd350be6ca5bdeae1676
f0rsakeN-afk <nar3sh0@gmail.com>
2025-01-10 23:19:49 +0545

    fix merge conflict

--- frontend/src/data/SyllabusData.ts
+++ frontend/src/data/SyllabusData.ts
@@ -41,24 +41,12 @@ export const SyllabusData: SyllabusTypes[] = [
     id: 7,
     name: "First semester Computer",
     branch: "Computer",
-<<<<<<< HEAD
-    url: "https://drive.google.com/file/d/1bnbt88xp47f9pBbwGEcoSEYgtTFmBMcP/view?usp=drive_link"
-=======
     url: "https://drive.google.com/file/d/1bnbt88xp47f9pBbwGEcoSEYgtTFmBMcP/view?usp=drive_link",
->>>>>>> 8792b3c (fix bug)
   },
   {
     id: 8,
     name: "Fourth semester Computer",
     branch: "Computer",
-<<<<<<< HEAD
-    url: "https://drive.google.com/file/d/1LEIQENg0sphJyKBpAxz1OEHp1k-l1XF-/view?usp=drive_link"
-  },{
-    id: 9,
-    name: "First semester Civil",
-    branch: "Civil",
-    url: "https://drive.google.com/file/d/1_6X2IRVK328CEp2448MSW8rUHKm8uOy8/view?usp=drive_link"
-=======
     url: "https://drive.google.com/file/d/1LEIQENg0sphJyKBpAxz1OEHp1k-l1XF-/view?usp=drive_link",
   },
   {
@@ -66,6 +54,5 @@ export const SyllabusData: SyllabusTypes[] = [
     name: "First semester Civil",
     branch: "Civil",
     url: "https://drive.google.com/file/d/1_6X2IRVK328CEp2448MSW8rUHKm8uOy8/view?usp=drive_link",
->>>>>>> 8792b3c (fix bug)
   },
 ];
