18b8da1b95b0af8ded0e27afa7c7bc247e2972c2
f0rsakeN-afk <nar3sh0@gmail.com>
2025-02-24 00:02:14 +0545

    added seo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,39 @@
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Loader from "./components/Loader";
 import PDF from "./pages/PDF";
 import ScrollToTop from "./components/ScrollToTop";
+import { HelmetProvider } from "react-helmet-async";
 const Home = lazy(() => import("./pages/Home"));
 const Layout = lazy(() => import("./components/Layout"));
-const About = lazy(() => import('./pages/About'))
+const About = lazy(() => import("./pages/About"));
 const PageNotFound = lazy(() => import("./pages/PageNotFound"));
-const PastQuestions = lazy(() => import('./pages/PastQuestions'));
-const Assignments = lazy(() => import('./pages/Assignments'));
-const Syllabus = lazy(() => import('./pages/Syllabus'));
+const PastQuestions = lazy(() => import("./pages/PastQuestions"));
+const Assignments = lazy(() => import("./pages/Assignments"));
+const Syllabus = lazy(() => import("./pages/Syllabus"));
 
 const App = () => {
   return (
-    <Suspense fallback={<Loader />}>
-      <Analytics />
-      <BrowserRouter>
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/pdfs" element={<PDF />} />
-            <Route path="/pastquestions" element={<PastQuestions />} />
-            <Route path="/assignments" element={<Assignments />} />
-            <Route path="/syllabus" element={<Syllabus />} />
-          </Route>
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </Suspense>
+    <HelmetProvider>
+      <Suspense fallback={<Loader />}>
+        <Analytics />
+        <BrowserRouter>
+          <ScrollToTop />
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/pdfs" element={<PDF />} />
+              <Route path="/pastquestions" element={<PastQuestions />} />
+              <Route path="/assignments" element={<Assignments />} />
+              <Route path="/syllabus" element={<Syllabus />} />
+            </Route>
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
+    </HelmetProvider>
   );
 };
 
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -15,6 +15,7 @@ import { fadeInUpVariants, staggerContainerVariants } from "@/utils/animation";
 
 //get subject type and data
 import { Subject, subjectData } from "@/data/SubjectData";
+import SEO from "@/components/SEO";
 
 const semesters = Array.from({ length: 8 }, (_, i) => `Semester ${i + 1}`);
 const branches = [
@@ -62,7 +63,6 @@ const Home = () => {
       setGreeting("Good night! 🌙");
       setDayMessage("Sweet dreams! 🌙💤");
     }
-
   }, []);
 
   useEffect(() => {
@@ -81,166 +81,168 @@ const Home = () => {
   }, [selectedBranch, selectedSemester]);
 
   return (
-    <motion.div
-      className="min-h-screen"
-      initial="hidden"
-      animate="visible"
-      variants={staggerContainerVariants}
-    >
-      <section className="relative h-screen flex items-center overflow-hidden">
-        {/* Simple Gradient Background */}
-        <motion.div
-          className="absolute inset-0 bg-gradient-to-br from-blue-500   to-purple-600 dark:from-slate-800 dark:to-slate-950"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 1 }}
-        />
-
-        {/* Main Content */}
-        <div className="relative w-full max-w-7xl mx-auto px-4">
-          {/* Hero Text */}
+    <>
+      <SEO />
+      <motion.div
+        className="min-h-screen"
+        initial="hidden"
+        animate="visible"
+        variants={staggerContainerVariants}
+      >
+        <section className="relative h-screen flex items-center overflow-hidden">
+          {/* Simple Gradient Background */}
           <motion.div
-            className="text-center mb-16"
-            variants={staggerContainerVariants}
-          >
-            <motion.h1
-              className="text-4xl md:text-6xl font-bold text-white mb-6"
-              variants={fadeInUpVariants}
+            className="absolute inset-0 bg-gradient-to-br from-blue-500   to-purple-600 dark:from-slate-800 dark:to-slate-950"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+          />
+
+          {/* Main Content */}
+          <div className="relative w-full max-w-7xl mx-auto px-4">
+            {/* Hero Text */}
+            <motion.div
+              className="text-center mb-16"
+              variants={staggerContainerVariants}
             >
-              <section>
-                <h2 className="pb-4 text-3xl ">
-                  {greeting} {dayMessage}
-                </h2>
-              </section>
+              <motion.h1
+                className="text-4xl md:text-6xl font-bold text-white mb-6"
+                variants={fadeInUpVariants}
+              >
+                <section>
+                  <h2 className="pb-4 text-3xl ">
+                    {greeting} {dayMessage}
+                  </h2>
+                </section>
+                Engineering Study Materials
+                <br />
+                All in One Place
+              </motion.h1>
+              <motion.p
+                className="text-xl text-gray-100 mb-8 max-w-2xl mx-auto"
+                variants={fadeInUpVariants}
+              >
+                Access thousands of engineering PDFs, notes, and study materials
+              </motion.p>
+            </motion.div>
 
-              Engineering Study Materials
-              <br />
-              All in One Place
-            </motion.h1>
-            <motion.p
-              className="text-xl text-gray-100 mb-8 max-w-2xl mx-auto"
+            {/* Search Card */}
+            <motion.div
+              className="max-w-4xl mx-auto bg-white/10  rounded-xl shadow-2xl p-8 border border-white/20"
               variants={fadeInUpVariants}
+              //whileHover={{ scale: 1.02 }}
+              transition={{ type: "spring", stiffness: 300 }}
             >
-              Access thousands of engineering PDFs, notes, and study materials
-            </motion.p>
-          </motion.div>
-
-          {/* Search Card */}
-          <motion.div
-            className="max-w-4xl mx-auto bg-white/10  rounded-xl shadow-2xl p-8 border border-white/20"
-            variants={fadeInUpVariants}
-            //whileHover={{ scale: 1.02 }}
-            transition={{ type: "spring", stiffness: 300 }}
-          >
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-              {/* Semester Selection */}
-              <motion.div className="space-y-2" variants={fadeInUpVariants}>
-                <label className="text-sm font-medium text-white/90">
-                  Select Semester
-                </label>
-                <Select
-                  onValueChange={setSelectedSemester}
-                  value={selectedSemester}
-                >
-                  <SelectTrigger className="bg-white/10 border-white/20 text-white">
-                    <SelectValue placeholder="Choose Semester" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {semesters.map((semester) => (
-                      <SelectItem key={semester} value={semester}>
-                        {semester}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
-              </motion.div>
-
-              {/* Branch Selection */}
-              <motion.div className="space-y-2" variants={fadeInUpVariants}>
-                <label className="text-sm font-medium text-white/90">
-                  Select Branch
-                </label>
-                <Select
-                  onValueChange={setSelectedBranch}
-                  value={selectedBranch}
-                >
-                  <SelectTrigger className="bg-white/10 border-white/20 text-white">
-                    <SelectValue placeholder="Choose Branch" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {branches.map((branch) => (
-                      <SelectItem key={branch} value={branch}>
-                        {branch}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
-              </motion.div>
-
-              {/* Subject selection */}
-              <motion.div variants={fadeInUpVariants} className="space-y-2">
-                <label className="text-sm font-medium text-white/90">
-                  Select Subject
-                </label>
-                <Select
-                  onValueChange={setSelectedSubject}
-                  value={selectedSubject}
-                >
-                  <SelectTrigger className="bg-white/10 border-white/20 text-white">
-                    <SelectValue placeholder="Choose Subject" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {subject &&
-                      subject?.map((s) => (
-                        <SelectItem key={s.name} value={s.name}>
-                          {s.name}
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                {/* Semester Selection */}
+                <motion.div className="space-y-2" variants={fadeInUpVariants}>
+                  <label className="text-sm font-medium text-white/90">
+                    Select Semester
+                  </label>
+                  <Select
+                    onValueChange={setSelectedSemester}
+                    value={selectedSemester}
+                  >
+                    <SelectTrigger className="bg-white/10 border-white/20 text-white">
+                      <SelectValue placeholder="Choose Semester" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {semesters.map((semester) => (
+                        <SelectItem key={semester} value={semester}>
+                          {semester}
                         </SelectItem>
                       ))}
-                  </SelectContent>
-                </Select>
-              </motion.div>
-            </div>
+                    </SelectContent>
+                  </Select>
+                </motion.div>
 
-            <motion.div
-              variants={fadeInUpVariants}
-              //whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 0.95 }}
-            >
-              <Button
-                className="w-full mt-6 bg-white/10 hover:bg-white/20 text-white border border-white/20
-                           transition-all duration-300"
-                disabled={
-                  !selectedSemester || !selectedBranch || !selectedSubject
-                }
-                onClick={handleSearch}
+                {/* Branch Selection */}
+                <motion.div className="space-y-2" variants={fadeInUpVariants}>
+                  <label className="text-sm font-medium text-white/90">
+                    Select Branch
+                  </label>
+                  <Select
+                    onValueChange={setSelectedBranch}
+                    value={selectedBranch}
+                  >
+                    <SelectTrigger className="bg-white/10 border-white/20 text-white">
+                      <SelectValue placeholder="Choose Branch" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {branches.map((branch) => (
+                        <SelectItem key={branch} value={branch}>
+                          {branch}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </motion.div>
+
+                {/* Subject selection */}
+                <motion.div variants={fadeInUpVariants} className="space-y-2">
+                  <label className="text-sm font-medium text-white/90">
+                    Select Subject
+                  </label>
+                  <Select
+                    onValueChange={setSelectedSubject}
+                    value={selectedSubject}
+                  >
+                    <SelectTrigger className="bg-white/10 border-white/20 text-white">
+                      <SelectValue placeholder="Choose Subject" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {subject &&
+                        subject?.map((s) => (
+                          <SelectItem key={s.name} value={s.name}>
+                            {s.name}
+                          </SelectItem>
+                        ))}
+                    </SelectContent>
+                  </Select>
+                </motion.div>
+              </div>
+
+              <motion.div
+                variants={fadeInUpVariants}
+                //whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
               >
-                Search PDFs
-              </Button>
+                <Button
+                  className="w-full mt-6 bg-white/10 hover:bg-white/20 text-white border border-white/20
+                           transition-all duration-300"
+                  disabled={
+                    !selectedSemester || !selectedBranch || !selectedSubject
+                  }
+                  onClick={handleSearch}
+                >
+                  Search PDFs
+                </Button>
+              </motion.div>
             </motion.div>
-          </motion.div>
-        </div>
-      </section>
+          </div>
+        </section>
 
-      {/* Features */}
-      <motion.div
-        variants={fadeInUpVariants}
-        viewport={{ once: true }}
-        whileInView="visible"
-        initial="hidden"
-      >
-        <Features />
-      </motion.div>
+        {/* Features */}
+        <motion.div
+          variants={fadeInUpVariants}
+          viewport={{ once: true }}
+          whileInView="visible"
+          initial="hidden"
+        >
+          <Features />
+        </motion.div>
 
-      {/* recent */}
-      <motion.div
-        variants={fadeInUpVariants}
-        viewport={{ once: true }}
-        whileInView="visible"
-        initial="hidden"
-      >
-        <Recent />
+        {/* recent */}
+        <motion.div
+          variants={fadeInUpVariants}
+          viewport={{ once: true }}
+          whileInView="visible"
+          initial="hidden"
+        >
+          <Recent />
+        </motion.div>
       </motion.div>
-    </motion.div>
+    </>
   );
 };
 
