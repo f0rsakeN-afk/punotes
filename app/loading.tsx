af4732d98d1f7c31b227dc21a833b2296b6d6fcf
f0rsakeN-afk <nar3sh0@gmail.com>
2025-12-16 09:22:38 +0545

    change loader

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,19 +1,18 @@
 import "./loader.css";
 
 export default function Loading() {
-  const dots = Array.from({ length: 15 });
-
   return (
-    <div role="status" aria-label="Loading" className="h-dvh flex items-center justify-center">
-      <aside className="container-loader">
-        {dots.map((_, index) => (
-          <div
-            key={index}
-            className="aro"
-            style={{ "--s": index } as React.CSSProperties}
-          ></div>
-        ))}
-      </aside>
+    <div
+      role="status"
+      aria-label="Loading"
+      className="h-dvh flex items-center justify-center"
+    >
+      <div className="loader">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </div>
   );
 }
